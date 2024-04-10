fd011368fffc3e92807dfb3edc40024d8de978dd
rslangl <fjellape1447@protonmail.com>
2024-04-10 21:41:09 +0200

    feat: added neovim install

--- scripts/7-dev_tools.sh
+++ scripts/7-dev_tools.sh
@@ -16,9 +16,21 @@ chown -R user:user /opt/vcpkg
 cd /opt/vcpkg && ./bootstrap-vcpkg.sh -disableMetrics
 ln -s /opt/vcpkg/vcpkg /usr/local/bin/vcpkg
 
+# TODO: Install virtualization tools
+# VirtualBox
+# Dokcer
+
 # Install IaC tools
 apt install -y \
 	ansible \
 	vagrant
 
-# TODO: Install neovim
+# Install packages required for neovim
+apt install -y \
+	luajit \
+	ripgrep \
+	unzip
+
+# Install neovim from source
+git clone https://github.com/neovim/neovim /home/user/src && cd /home/user/src/neovim
+make CMAKE_BUILD_TYPE=Release && make install
