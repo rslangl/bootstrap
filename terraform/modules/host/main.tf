1af531e0d086b3c14f4da8b6d154cff40315e6f0
rslangl <fjellape1447@protonmail.com>
2025-04-26 16:21:34 +0200

    fix: recursive load of same module

--- terraform/modules/host/main.tf
+++ terraform/modules/host/main.tf
@@ -1,3 +1,11 @@
-module "repos" {
-  source = "./"
+resource "proxmox_virtual_environment_apt_standard_repository" "standard" {
+  handle = "no-subscription"
+  node   = "pve"
+}
+
+resource "proxmox_virtual_environment_apt_repository" "standard" {
+  enabled   = true
+  file_path = proxmox_virtual_environment_apt_standard_repository.example.file_path
+  index     = proxmox_virtual_environment_apt_standard_repository.example.index
+  node      = proxmox_virtual_environment_apt_standard_repository.example.node
 }
