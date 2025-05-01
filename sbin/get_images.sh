14bb09dfa701feb66d14c7b7ff59c771d35a9b4a
rslangl <fjellape1447@protonmail.com>
2025-05-01 20:04:53 +0200

    chore: add ubuntu cloud image

--- sbin/get_images.sh
+++ sbin/get_images.sh
@@ -7,11 +7,13 @@ ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 DOWNLOAD_DIR="${ROOT_DIR}/images"
 
 PVE_URL="https://enterprise.proxmox.com/iso/proxmox-ve_8.4-1.iso|d237d70ca48a9f6eb47f95fd4fd337722c3f69f8106393844d027d28c26523d8"
+UBUNTU_URL="https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img|1ecb0f1fbb7d722987e498cc5cdb8a3221c09c9501d02cdbba31fb8097e5349b"
 
 declare -A IMAGES
 
 IMAGES=(
   [pve]="$PVE_URL"
+  [ubuntu]="$UBUNTU_URL"
 )
 
 for image in "${!IMAGES[@]}"; do
@@ -24,7 +26,7 @@ for image in "${!IMAGES[@]}"; do
   if [[ $? -eq 0 ]]; then
     echo "Saved image as $dest_file"
 
-    echo "Verifying checksum..."z
+    echo "Verifying checksum..."
     actual_checksum=$(sha256sum "$dest_file" | awk '{print $1}')
 
     if [[ $actual_checksum == $checksum ]]; then
