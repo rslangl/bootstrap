3ffd4ecb4d4d9bac3548cfe8e28e51dbe762e944
rslangl <fjellape1447@protonmail.com>
2025-05-02 21:49:04 +0200

    feat: add libvirt defined network and spinup script

---
+++ test/spinup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -ex
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+IMAGES_DIR="${ROOT_DIR}/images"
+
+declare -A HOSTS
+
+HOSTS=(
+  [pve]="192.168.100.10|52:54:00:aa:bb:01"
+)
+
+# launch the network to be used by the VMs
+virsh net-define test/testnet.xml
+virsh net-autostart testnet
+virsh net-start testnet
+
+# launch VMs
+for host in "${!HOSTS[@]}"; do
+  IFS="|" read -r ip mac <<<"${IMAGES[$host]}"
+
+  virt-install \
+    --name "${host}-local" \
+    --ram 2048 \
+    --vcpus 2 \
+    --disk path="${IMAGES_DIR}/${host}.img",format=raw \
+    --graphics none \
+    --network network=testnet,mac=$mac \
+    --console pty,target_type=serial \
+    --extra-args 'console=ttyS0,115200n8 serial'
+
+done
