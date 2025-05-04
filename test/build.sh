591d072118ee61ed2f0efcf2ef1b4598af6590b7
rslangl <fjellape1447@protonmail.com>
2025-05-04 21:45:14 +0200

    chore: typo fix and added image build cmd

--- test/build.sh
+++ test/build.sh
@@ -6,8 +6,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 IMAGES_DIR="${ROOT_DIR}/images"
 
+docker build -t proxmox-iso-builder -f Dockerfile.pve-autoinstaller-builder .
+
 docker run --rm \
-  -v "$(pwd)/${IMAGES_DIR]/pve.iso:/build/pve.iso:ro" \
+  -v "$(pwd)/${IMAGES_DIR}/pve.iso:/build/pve.iso:ro" \
   -v "$(pwd)/answer.toml:/build/answer.toml:ro" \
   -v "$(pwd)/output:/build/output" \
   proxmox-iso-builder /build/pve.iso --fetch-from iso --answer-file /build/answer.toml \
