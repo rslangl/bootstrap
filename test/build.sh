f1c6c7bc3de0215f813a3bfb97539d42c6516cf2
rslangl <fjellape1447@protonmail.com>
2025-05-04 21:40:32 +0200

    feat: add builder for PVE autoinstaller

---
+++ test/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+IMAGES_DIR="${ROOT_DIR}/images"
+
+docker run --rm \
+  -v "$(pwd)/${IMAGES_DIR]/pve.iso:/build/pve.iso:ro" \
+  -v "$(pwd)/answer.toml:/build/answer.toml:ro" \
+  -v "$(pwd)/output:/build/output" \
+  proxmox-iso-builder /build/pve.iso --fetch-from iso --answer-file /build/answer.toml \
+  --output /build/output/pve-auto.iso
--- test/spinup.sh
+++ test/spinup.sh
@@ -9,7 +9,7 @@ IMAGES_DIR="${ROOT_DIR}/images"
 declare -A HOSTS
 
 HOSTS=(
-  [pve]="192.168.100.10|52:54:00:aa:bb:01"
+  [pve]="2048|2|192.168.100.10|52:54:00:aa:bb:01|${IMAGES_DIR}/pve-auto.iso"
 )
 
 # launch the network to be used by the VMs
@@ -19,13 +19,13 @@ virsh net-start testnet
 
 # launch VMs
 for host in "${!HOSTS[@]}"; do
-  IFS="|" read -r ip mac <<<"${IMAGES[$host]}"
+  IFS="|" read -r ram vcpu ip mac iso <<<"${IMAGES[$host]}"
 
   virt-install \
     --name "${host}-local" \
-    --ram 2048 \
-    --vcpus 2 \
-    --disk path="${IMAGES_DIR}/${host}.img",format=raw \
+    --ram $ram \
+    --vcpus $vcpu \
+    --disk path="$iso",format=raw \
     --graphics none \
     --network network=testnet,mac=$mac \
     --console pty,target_type=serial \
