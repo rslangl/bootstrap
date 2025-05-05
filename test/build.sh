f6531f7fdfc52ec5a6538121284c7df74a20291f
rslangl <fjellape1447@protonmail.com>
2025-05-05 21:01:32 +0200

    chore: inline todos for future refactoring

--- test/build.sh
+++ test/build.sh
@@ -6,8 +6,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 IMAGES_DIR="${ROOT_DIR}/images"
 
+# TODO: check if image already exists
 docker build -t proxmox-iso-builder -f Dockerfile.pve-autoinstaller-builder .
 
+# TODO: check if ISO already exists
 docker run --rm \
   -v "$(pwd)/${IMAGES_DIR}/pve.iso:/build/pve.iso:ro" \
   -v "$(pwd)/answer.toml:/build/answer.toml:ro" \
