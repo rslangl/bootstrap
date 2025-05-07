45e4cd1886bec450ae5e794faf1827686c7f1c5f
rslangl <fjellape1447@protonmail.com>
2025-05-07 20:08:26 +0200

    feat: add checks on test network

--- test/spinup.sh
+++ test/spinup.sh
@@ -6,29 +6,45 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 IMAGES_DIR="${ROOT_DIR}/images"
 
+NETWORK_NAME="testnet"
+
 declare -A HOSTS
 
 HOSTS=(
   [pve]="2048|2|192.168.100.10|52:54:00:aa:bb:01|${IMAGES_DIR}/pve-auto.iso"
 )
 
-# launch the network to be used by the VMs
-virsh net-define test/testnet.xml
-virsh net-autostart testnet
-virsh net-start testnet
+# checks whether the test network is running
+if virsh net-info "$NETWORK_NAME" &> /dev/null; then
+  echo "Test network $NETWORK_NAME exists"
+  if virsh net-info "$NETWORK_NAME" | grep -q '^Active:\s*yes'; then
+    echo "Test network $NETWORK_NAME is running"
+  else
+    echo "Test network $NETWORK_NAME not running, starting..."
+    virsh net-start "$NETWORK_NAME"
+  end
+else
+  virsh net-define "${SCRIPT_DIR}/testnet.xml"
+  virsh net-autostart testnet
+  virsh net-start testnet
+end
 
-# launch VMs
+# iterates list of VMs and ensures they are in a started state
 for host in "${!HOSTS[@]}"; do
-  IFS="|" read -r ram vcpu ip mac iso <<<"${IMAGES[$host]}"
+  IFS="|" read -r ram vcpu ip mac iso <<<"${HOSTS[$host]}"
+
+  #qemu-img create -f qcow2 $host.qcow2 10G
 
   virt-install \
+    --osinfo debian12 \
     --name "${host}-local" \
-    --ram $ram \
-    --vcpus $vcpu \
-    --disk path="$iso",format=raw \
+    --ram "$ram" \
+    --vcpus "$vcpu" \
+    --disk path="$host".qcow2 \
+    --cdrom "$iso" \
     --graphics none \
     --network network=testnet,mac=$mac \
-    --console pty,target_type=serial \
-    --extra-args 'console=ttyS0,115200n8 serial'
+    --console pty,target_type=serial
+  #--extra-args 'console=ttyS0,115200n8 serial'
 
 done
