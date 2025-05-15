22ad5c5bdc998ee8ff83ca3457928c0cab629250
rslangl <fjellape1447@protonmail.com>
2025-05-15 18:09:07 +0200

    fix: checksum check

--- sbin/get_images.sh
+++ sbin/get_images.sh
@@ -6,15 +6,15 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 DOWNLOAD_DIR="${ROOT_DIR}/images"
 
-PVE_URL="https://enterprise.proxmox.com/iso/proxmox-ve_8.4-1.iso|d237d70ca48a9f6eb47f95fd4fd337722c3f69f8106393844d027d28c26523d8"
-#UBUNTU_URL="https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img|1ecb0f1fbb7d722987e498cc5cdb8a3221c09c9501d02cdbba31fb8097e5349b"
-#OPNSENSE_URL="https://opnsense-mirror.hiho.ch/releases/mirror/OPNsense-25.1-dvd-amd64.iso.bz2|68efe0e5c20bd5fbe42918f000685ec10a1756126e37ca28f187b2ad7e5889ca"
+PVE_SRC="https://enterprise.proxmox.com/iso/proxmox-ve_8.4-1.iso|d237d70ca48a9f6eb47f95fd4fd337722c3f69f8106393844d027d28c26523d8"
+UBUNTU_SRC="https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img|1ecb0f1fbb7d722987e498cc5cdb8a3221c09c9501d02cdbba31fb8097e5349b"
+OPNSENSE_SRC="https://opnsense-mirror.hiho.ch/releases/mirror/OPNsense-25.1-dvd-amd64.iso.bz2|68efe0e5c20bd5fbe42918f000685ec10a1756126e37ca28f187b2ad7e5889ca"
 
 declare -A IMAGES
 
 IMAGES=(
-  [pve]="$PVE_URL"
-  [ubuntu]="$UBUNTU_URL"
+  [pve]="$PVE_SRC"
+  #[ubuntu]="$UBUNTU_URL"
 )
 
 for image in "${!IMAGES[@]}"; do
@@ -35,6 +35,8 @@ for image in "${!IMAGES[@]}"; do
       # echo "Converting to img..."
       # qemu-img convert -f raw -O raw "$dest_file" "${DOWNLOAD_DIR}/${image}.img"
       # echo "Done!"
+    else
+      echo "WARNING: Checksum failed! Expected $checksum but got $actual_checksum"
     fi
   else
     echo "Failed to download from $url"
