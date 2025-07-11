9876dfc6fbdc168b3d53d3a34871b44881cebcb0
rslangl <fjellape1447@protonmail.com>
2025-07-11 19:18:18 +0200

    chore: update host configs to include KVM host

--- host_configs/generate.mk
+++ host_configs/generate.mk
@@ -2,9 +2,11 @@
 
 host_configs.clean:
 	@echo "Cleaning KCL output in ..."
-	rm -f $(HOSTS_DIR)/router/host_vars/router.yaml
+	rm -f $(HOSTS_DIR)/router/ansible/host_vars/router.yaml
+	rm -f $(HOSTS_DIR)/kvm/ansible/host_vars/kvm.yaml
 
 host_configs.generate:
 	@echo "Generating host_vars files..."
-	kcl run $(HOSTS_CONFIG_DIR)/hosts/router.k --output $(HOSTS_DIR)/router/host_vars/router.yaml
+	kcl run $(HOSTS_CONFIG_DIR)/hosts/router.k --output $(HOSTS_DIR)/router/ansible/host_vars/router.yaml
+	kcl run $(HOSTS_CONFIG_DIR)/hosts/kvm.k --output $(HOSTS_DIR)/kvm/ansible/host_vars/kvm.yaml
 
