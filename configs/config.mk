fcecfbc931588ed6ea4db091f885fda7b7d31f57
rslangl <fjellape1447@protonmail.com>
2025-07-13 11:53:25 +0200

    feat: refactor and integrate make targets from root

--- configs/config.mk
+++ configs/config.mk
@@ -1,11 +1,11 @@
 .PHONY: host_configs.generate host_configs.clean
 
-host_configs.clean:
+cfg.clean:
 	@echo "Cleaning KCL output in ..."
 	rm -f $(HOSTS_DIR)/router/ansible/host_vars/router.yaml
 	rm -f $(HOSTS_DIR)/kvm/ansible/host_vars/kvm.yaml
 
-host_configs.generate:
+cfg.generate:
 	@echo "Generating host_vars files..."
 	kcl run $(HOSTS_CONFIG_DIR)/hosts/router.k --output $(HOSTS_DIR)/router/ansible/host_vars/router.yaml
 	kcl run $(HOSTS_CONFIG_DIR)/hosts/kvm.k --output $(HOSTS_DIR)/kvm/ansible/host_vars/kvm.yaml
--- liveOS/README.md
+++ liveOS/README.md
@@ -11,7 +11,7 @@ provider for a centralized provisioner component, i.e. PiKVM.
 * `includes.chroot`: Everything in here gets copied to the live system, for example:
   * `includes.chroot/srv/registry`: Container images
   * `includes.chroot/srv/apt`: Apt repo files
-  * `includes.chroot/srv/bsd`: BSD repo files 
+  * `includes.chroot/srv/bsd`: BSD repo files
   * `includes.chroot/usr/local/bin/startup.sh`: Custom startup script
 * `hooks/live.chroot`: Configures services or startup scripts, e.g. docker, nginx
 
