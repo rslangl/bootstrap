0980151a5a991626ca5ebfe92b62b51d5cf863a6
rslangl <fjellape1447@protonmail.com>
2025-07-14 21:57:30 +0200

    chore: add TF providers endpoint in nginx

--- liveOS/os.mk
+++ liveOS/os.mk
@@ -43,12 +43,13 @@ os.tf:
 	@echo "Fetching Terraform providers..."
 	docker build -t tf-mirror -f $(LIVE_BUILD_DIR)/docker/tf-providers/Dockerfile $(LIVE_BUILD_DIR)/docker/tf-providers
 	docker run --rm -v $(LIVE_BUILD_DIR)/build-artifacts/tf-providers:/mirror tf-mirror
+	docker container stop local_registry && docker container rm local_registry
 
 os.build: os.apt os.img os.tf
 	@echo "Copying resources to live OS image paths..."
 	cp -r build-artifacts/aptrepo live-build/config/includes.chroot/srv/
 	cp -r build-artifacts/registry_data live-build/config/includes.chroot/srv/
-	cp -r build-artifacts/registry.tar live-build/config/includes.chroot/srv/
+	cp -r build-artifacts/registry/registry.tar live-build/config/includes.chroot/srv/
 	cp -r build-artifacts/tf-providers live-build/config/includes.chroot/srv
 	@echo "Building live OS image..."
 	docker build -t $(CONTAINER_NAME) .
