08ee7d8c56db8f6e528799371759222e42bd433e
rslangl <fjellape1447@protonmail.com>
2025-07-14 19:00:00 +0200

    chore: add TF provider image build target and fixed build paths

--- liveOS/os.mk
+++ liveOS/os.mk
@@ -14,16 +14,16 @@ os.clean:
 
 os.deb-fetch:
 	@echo "Fetching apt repository packages..."
-	docker build -t apt-deb-fetcher docker/apt-deb-fetcher
+	docker build -t apt-deb-fetcher -f $(LIVE_BUILD_DIR)/docker/apt-deb-fetcher/Dockerfile $(LIVE_BUILD_DIR)/docker/apt-deb-fetcher
 	docker run --rm \
-		-v $(PWD)/build-artifacts/aptrepo/packages:/workdir/packages \
-		-v $(PWD)/docker/apt-deb-fetcher/packages.txt:/workdir/packages.txt \
+		-v $(LIVE_BUILD_DIR)/build-artifacts/aptrepo/packages:/workdir/packages \
+		-v $(LIVE_BUILD_DIR)/docker/apt-deb-fetcher/packages.txt:/workdir/packages.txt \
 		apt-deb-fetcher
 
 os.apt: os.deb-fetch
 	@echo "Building apt repository..."
-	docker build -t apt-repo-builder docker/apt-repo-builder/Dockerfile
-	docker run --rm -v $(PWD)/build-artifacts/aptrepo:/repo apt-repo-builder
+	docker build -t apt-repo-builder -f $(LIVE_BUILD_DIR)/docker/apt-repo-builder/Dockerfile $(LIVE_BUILD_DIR)/docker/apt-repo-builder
+	docker run --rm -v $(LIVE_BUILD_DIR)/build-artifacts/aptrepo:/repo apt-repo-builder
 
 os.img-fetch:
 	@echo "Fetching container images..."
@@ -42,11 +42,17 @@ os.img: os.img-fetch
 	docker cp registry:/var/lib/registry -v $(PWD)/build-artifacts/registry_data
 	docker save registry:2 -o $(PWD)/build-artifacts/registry.tar
 
-os.build: os.apt os.img
+os.tf:
+	@echo "Fetching Terraform providers..."
+	docker build -t tf-mirror -f $(LIVE_BUILD_DIR)/docker/tf-providers/Dockerfile $(LIVE_BUILD_DIR)/docker/tf-providers
+	docker run --rm -v $(LIVE_BUILD_DIR)/build-artifacts/tf-providers:/mirror tf-mirror
+
+os.build: os.apt os.img os.tf
 	@echo "Copying resources to live OS image paths..."
 	cp -r build-artifacts/aptrepo live-build/config/includes.chroot/srv/
 	cp -r build-artifacts/registry_data live-build/config/includes.chroot/srv/
 	cp -r build-artifacts/registry.tar live-build/config/includes.chroot/srv/
+	cp -r build-artifacts/tf-providers live-build/config/includes.chroot/srv
 	@echo "Building live OS image..."
 	docker build -t $(CONTAINER_NAME) .
 	@echo "Running live OS image..."
