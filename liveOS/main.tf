75201742c4ce91a0ebc46e7209184da804666ae8
rslangl <151566232+rslangl@users.noreply.github.com>
2025-08-07 21:41:53 +0200

    chore: BSD image in resource builder (#52)
    * chore: add base docker structure
    
    * fix: docker build
    
    * chore: remove bsd build dir
    
    * chore: add starting point for BSD VM for fetching resources
    
    * chore: add freebsd image to downloadable resources
    
    * fix: add missing freebsd entry
    
    * chore: add TF manifest for BSD image with cloudinit

---
+++ liveOS/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_version = ">= 1.12.0"
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+      version = "0.8.3"
+    }
+  }
+  backend "local" {
+    path = "../.cache/tfstate/sandbox/terraform.tfstate"
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+resource "libvirt_pool" "resourcebuilder_pool" {
+  name = "resourcesbuilder"
+  type = "dir"
+  target {
+    path = "../.cache/libvirt/pool"
+  }
+}
+
+# -------------------------------
+#   ISO paths
+# -------------------------------
+
+variable "freebsd_iso_path" {
+  type = string
+}
+
+# -------------------------------
+#   BSD resources VM
+# -------------------------------
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+}
+
+resource "libvirt_cloudinit_disk" "cloudinit" {
+  name = "cloudinit.iso"
+  pool = libvirt_pool.resourcebuilder_pool.name
+  user_data = data.template_file.user_data.rendered
+}
+
+resource "libvirt_domain" "resourcebuilder_freebsd" {
+  name = "freebsd"
+  memory = 2048
+  vcpu = 2
+  cloudinit = libvirt_cloudinit_disk.cloudinit.id
+  disk {
+    file = libvirt_volume.resourcebuilder_freebsd_disk.id
+  }
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+  autostart = true
+}
+
+resource "libvirt_volume" "resourcebuilder_freebsd_disk" {
+  name = "freebsd.qcow2"
+  pool = libvirt_pool.resourcebuilder_pool.name
+  #format = qcow2
+}
+
+# -------------------------------
+#   Apt resources VM
+# -------------------------------
+
+# TODO:
