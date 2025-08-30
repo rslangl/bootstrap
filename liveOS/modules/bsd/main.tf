99279b14df99abea804d4a889bb482e1c81b8cd9
rslangl <fjellape1447@protonmail.com>
2025-08-30 17:13:05 +0200

    chore: disable cloudinit block for BSD

--- liveOS/modules/bsd/main.tf
+++ liveOS/modules/bsd/main.tf
@@ -41,11 +41,11 @@ resource "libvirt_pool" "bsd_pool" {
   }
 }
 
-resource "libvirt_cloudinit_disk" "cloudinit" {
-  name = "cloudinit.iso"
-  pool = libvirt_pool.bsd_pool.name
-  user_data = data.template_file.user_data.rendered
-}
+# resource "libvirt_cloudinit_disk" "cloudinit" {
+#   name = "cloudinit.iso"
+#   pool = libvirt_pool.bsd_pool.name
+#   user_data = data.template_file.user_data.rendered
+# }
 
 resource "libvirt_volume" "bsd_disk" {
   name = "freebsd.qcow2"
@@ -57,7 +57,7 @@ resource "libvirt_domain" "bsd" {
   name = "freebsd"
   memory = 2048
   vcpu = 2
-  cloudinit = libvirt_cloudinit_disk.cloudinit.id
+  #cloudinit = libvirt_cloudinit_disk.cloudinit.id
   disk {
     file = libvirt_volume.bsd_disk.id
   }
