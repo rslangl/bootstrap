6a0c77375e24f51559bcaffcafd1bdb72491d97a
rslangl <fjellape1447@protonmail.com>
2025-10-19 22:42:28 +0200

    chore: cleanup liveos module var naming

--- liveOS/modules/liveos/main.tf
+++ liveOS/modules/liveos/main.tf
@@ -9,7 +9,8 @@ terraform {
   }
 }
 
-resource "null_resource" "copy_resources" {
+# Copy pre-provisioned resources to directory to be loaded by the ISO builder
+resource "null_resource" "liveos_system_copy_resources" {
   provisioner "local-exec" {
     interpreter = ["bash", "-c"]
     command = <<EOT
@@ -21,7 +22,8 @@ cp -r ${var.cache_dir}/build_artifacts/registry_data ${path.module}/config/inclu
   }
 }
 
-resource "docker_image" "liveos" {
+# Docker image for the live OS builder
+resource "docker_image" "liveos_system_image" {
   name = "liveos"
   keep_locally = false
   build {
@@ -31,19 +33,17 @@ resource "docker_image" "liveos" {
   }
 }
 
-resource "docker_container" "liveos" {
+# Docker container that builds the live OS image, where the output ISO
+# will be stored in ../../../.cache/output
+resource "docker_container" "liveos_system_container" {
   name = "liveos"
-  image = docker_image.liveos.image_id
+  image = docker_image.liveos_system_image.image_id
   rm = true
   privileged = true
   capabilities {
     add = ["SYS_ADMIN"]
   }
   security_opts = ["seccomp=unconfined"]
-  # volumes {
-  #   host_path = "${abspath(path.module)}/config"
-  #   container_path = "/var/livebuild/config/includes.chroot/srv"
-  # }
   volumes {
     host_path = "${var.cache_dir}/output"
     container_path = "/workdir/output"
