5b92ee93a4aa80adb5e7495152c88266b9936cdc
rslangl <fjellape1447@protonmail.com>
2025-10-24 22:43:58 +0200

    chore: add cleanup logic following image build

--- liveOS/os.mk
+++ liveOS/os.mk
@@ -1,8 +1,5 @@
 .PHONY: os.clean os.build
 
-#TF_DATA_DIR := $(shell mkdir -p ../.cache/tfdata && realpath -m ../.cache/tfdata)
-#TF_DATA_DIR := $(shell realpath ../.cache/tfdata)
-# TF_PLUGIN_CACHE_DIR := $(TF_DATA_DIR)/liveos/plugin-cache
 TF_PLAN := $(CACHE_DIR)/tfdata/liveos/plan
 TF_BIN := $(TOOLS_DIR)/terraform
 
@@ -30,3 +27,6 @@ os.build: os.init os.validate os.plan
 	@echo "Building live OS image..."
 	$(TF_BIN) -chdir=$(LIVEOS_DIR) apply -auto-approve -var="cache_dir=$(CACHE_DIR)" -var="scripts_dir=$(SBIN_DIR)" $(TF_PLAN)
 	docker logs liveos -f
+	@if [ -f $(CACHE_DIR)/output/liveUSB.iso ]; then \
+		$(TF_BIN) -chdir=$(LIVEOS_DIR) destroy -auto-approve \
+	fi
