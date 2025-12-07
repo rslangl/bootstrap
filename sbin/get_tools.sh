584c8ceda20a3a8272f2660b57777728861a3072
rslangl <fjellape1447@protonmail.com>
2025-12-07 16:17:06 +0100

    fix: lint

--- sbin/get_tools.sh
+++ sbin/get_tools.sh
@@ -9,17 +9,32 @@ CACHE_DIR="${ROOT_DIR}/.cache/tools/arm64"
 
 # docker: https://download.docker.com/linux/static/stable/x86_64/
 # QEMU: https://www.qemu.org/download/ (source only), github tags: https://api.github.com/repos/qemu/qemu/tags
-# tflint: https://github.com/terraform-linters/tflint/releases
-# ansible: https://api.github.com/repos/ansible/ansible/releases/latest, use tarball_url key
-# kcl: https://github.com/kcl-lang/cli/releases/download/v0.12.2-rc.1/kcl-v0.12.2-rc.1-linux-amd64.tar.gz
 
-# structure: ARM64
-TERRAFORM_SRC="https://releases.hashicorp.com/terraform/1.12.2/terraform_1.12.2_linux_arm64.zip"
+TERRAFORM_SRC_ARM64="https://releases.hashicorp.com/terraform/1.12.2/terraform_1.12.2_linux_arm64.zip"
+TFLINT_SRC_ARM64="https://github.com/terraform-linters/tflint/releases/download/v0.60.0/tflint_linux_arm64.zip"
+KCL_SRC_ARM64="https://github.com/kcl-lang/cli/releases/download/v0.12.1/kcl-v0.12.1-linux-arm64.tar.gz"
+ANSIBLE_SRC_ARM64="https://github.com/ansible/ansible/archive/refs/tags/v2.20.0.tar.gz"
 
-declare -A TOOLS
+TERRAFORM_SRC_AMD64="https://releases.hashicorp.com/terraform/1.12.2/terraform_1.12.2_linux_amd64.zip"
+TFLINT_SRC_AMD64="https://github.com/terraform-linters/tflint/releases/download/v0.60.0/tflint_linux_amd64.zip"
+KCL_SRC_AMD64="https://github.com/kcl-lang/cli/releases/download/v0.12.1/kcl-v0.12.1-linux-amd64.tar.gz"
+ANSIBLE_SRC_AMD64="https://github.com/ansible/ansible/archive/refs/tags/v2.20.0.tar.gz"
 
-TOOLS=(
-  [terraform]="$TERRAFORM_SRC"
+declare -A TOOLS_ARM64
+declare -A TOOLS_AMD64
+
+TOOLS_ARM64=(
+  [terraform]="$TERRAFORM_SRC_ARM64"
+  [tflint]="$TFLINT_SRC_ARM64"
+  [kcl]="$KCL_SRC_ARM64"
+  [ansible]="$ANSIBLE_SRC_ARM64"
+)
+
+TOOLS_AMD64=(
+  [terraform]="$TERRAFORM_SRC_AMD64"
+  [tflint]="$TFLINT_SRC_AMD64"
+  [kcl]="$KCL_SRC_AMD64"
+  [ansible]="$ANSIBLE_SRC_AMD64"
 )
 
 is_compressed() {
@@ -37,44 +52,60 @@ is_compressed() {
   esac
 }
 
-decompress_file() {
-  local compressed="$1"
-  local dest="$2"
+# decompress_file() {
+#   local compressed="$1"
+#   local dest="$2"
+#
+#   case "$compressed" in
+#   *.zip)
+#     unzip "$compressed" -d "$dest"
+#     ;;
+#   *)
+#     echo "ERROR: Unsupported compression type"
+#     exit 1
+#     ;;
+#   esac
+# }
 
-  case "$compressed" in
-  *.zip)
-    unzip "$compressed" -d "$dest"
-    ;;
-  *)
-    echo "ERROR: Unsupported compression type"
-    exit 1
-    ;;
-  esac
+download_tools() {
+  local tools="$1"
+
+  for file in "${tools[@]}"; do
+    IFS="|" read -r url <<<"${tools[$file]}"
+    clean_url="${url%%\?*}"
+    filename="${clean_url##*/}"
+    target_file="${DOWNLOAD_DIR}/${filename}"
+    tool_file="${CACHE_DIR}/arm64/${file}"
+
+    echo "Downloading from $url..."
+
+    curl -s -o "$target_file" "$url"
+
+    if is_compressed "$target_file"; then
+
+      case "$target_file" in
+        *.zip)
+          yes | unzip -o "$target_file" -d "$tool_file"
+          ;;
+        *.tar.xz)
+          tar -xJF "$target_file" -C "$tool_file"
+          ;;
+        *.tar.gz|*.tgz)
+          tar -xzf "$target_file" -C "$tool_file"
+          ;;
+        *)
+          echo "ERROR: Unsupported compression type"
+          exit 1
+          ;;
+      esac
+    fi
+
+    # TODO: if directory, handle accordingly
+
+  done
 }
 
 mkdir -p "${CACHE_DIR}"
 
-for file in "${!TOOLS[@]}"; do
-  IFS="|" read -r url <<<"${TOOLS[$file]}"
-  clean_url="${url%%\?*}"
-  filename="${clean_url##*/}"
-  target_file="${DOWNLOAD_DIR}/${filename}"
-  tool_file="${CACHE_DIR}/${file}"
-
-  echo "Downloading from $url..."
-
-  curl -s -o "$target_file" "$url"
-
-  if is_compressed "$target_file"; then
-
-    case "$target_file" in
-    *.zip)
-      yes | unzip -o "$target_file" -d "$tool_file"
-      ;;
-    *)
-      echo "ERROR: Unsupported compression type"
-      exit 1
-      ;;
-    esac
-  fi
-done
+download_tools "$TOOLS_ARM64"
+download_tools "$TOOLS_AMD64"
