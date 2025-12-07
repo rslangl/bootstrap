29766ae11f4a214fe10dce442d239d6de252d550
rslangl <fjellape1447@protonmail.com>
2025-12-07 18:52:58 +0100

    chore: add renovate versioning for aarch64 tools

--- sbin/get_tools_aarch64.sh
+++ sbin/get_tools_aarch64.sh
@@ -9,11 +9,22 @@ CACHE_DIR="${ROOT_DIR}/.cache/tools/arm64"
 
 # QEMU: https://www.qemu.org/download/ (source only), github tags: https://api.github.com/repos/qemu/qemu/tags
 
-TERRAFORM_SRC_ARM64="https://releases.hashicorp.com/terraform/1.12.2/terraform_1.12.2_linux_arm64.zip"
-TFLINT_SRC_ARM64="https://github.com/terraform-linters/tflint/releases/download/v0.60.0/tflint_linux_arm64.zip"
-KCL_SRC_ARM64="https://github.com/kcl-lang/cli/releases/download/v0.12.1/kcl-v0.12.1-linux-arm64.tar.gz"
-ANSIBLE_SRC_ARM64="https://github.com/ansible/ansible/archive/refs/tags/v2.20.0.tar.gz"
-DOCKER_SRC_ARM64="https://download.docker.com/linux/static/stable/aarch64/docker-29.1.2.tgz"
+# renovate: datasource=github-tags depName=ansible/ansible
+ANSIBLE_VERSION="2.20.0"
+# renovate: datasource=github-releases depName=kcl-lang/kcl
+KCL_VERSION="0.21.1"
+# renovate: datasource=github-releases depName=terraform-linters/tflint
+TFLINT_VERSION="0.60.0"
+# renovate: datasource=regex depName=terraform versioning=semver
+TERRAFORM_VERSION="1.12.2"
+# renovate: datasource=regex depName=docker versioning=semver
+DOCKER_VERSION="29.1.0"
+
+TERRAFORM_SRC_ARM64="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_arm64.zip"
+TFLINT_SRC_ARM64="https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_arm64.zip"
+KCL_SRC_ARM64="https://github.com/kcl-lang/cli/releases/download/v${KCL_VERSION}/kcl-v${KCL_VERSION}-linux-arm64.tar.gz"
+ANSIBLE_SRC_ARM64="https://github.com/ansible/ansible/archive/refs/tags/v${ANSIBLE_VERSION}.tar.gz"
+DOCKER_SRC_ARM64="https://download.docker.com/linux/static/stable/aarch64/docker-${DOCKER_VERSION}.tgz"
 
 declare -A TOOLS_ARM64
 
