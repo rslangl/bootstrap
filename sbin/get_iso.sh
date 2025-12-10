d2df237c21fbcd52f2c4575a397ff2d7e67ff6e5
rslangl <fjellape1447@protonmail.com>
2025-12-10 23:48:52 +0100

    fix: download images script

--- sbin/get_iso.sh
+++ sbin/get_iso.sh
@@ -2,10 +2,23 @@
 
 set -e
 
-#SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-#ROOT_DIR="$(dirname "$SCRIPT_DIR")"
-#DOWNLOAD_DIR="${ROOT_DIR}/.cache/tmp"
-#CACHE_DIR="${ROOT_DIR}/.cache/images"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
+DOWNLOAD_DIR="${ROOT_DIR}/.cache/tmp"
+CACHE_DIR="${ROOT_DIR}/.cache/images"
+
+FREEBSD_CLOUDINIT_VERSION="15.0"
+DEBIAN_CLOUDINIT_VERSION="13"
+
+FREEBSD_CLOUDINIT_SRC="https://download.freebsd.org/releases/VM-IMAGES/${FREEBSD_CLOUDINIT_VERSION}-RELEASE/amd64/Latest/FreeBSD-${FREEBSD_CLOUDINIT_VERSION}-RELEASE-amd64-BASIC-CLOUDINIT-zfs.qcow2.xz"
+DEBIAN_CLOUDINIT_SRC="https://cloud.debian.org/images/cloud/trixie/latest/debian-${DEBIAN_CLOUDINIT_VERSION}-generic-amd64.qcow2"
+
+declare -A IMAGES
+
+IMAGES=(
+  [freebsd]="${FREEBSD_CLOUDINIT_SRC}"
+  [debian]="${DEBIAN_CLOUDINIT_SRC}"
+)
 
 get_checksum() {
   local file="$1"
@@ -46,38 +59,49 @@ decompress_image() {
   esac
 }
 
-ISO_URL="$1"
-ISO_PATH="$2"
-EXPECTED_CHECKSUM="$3"
+mkdir -p "${CACHE_DIR}"
 
-if [ -f "$ISO_PATH" ]; then
-    echo "ISO already exists at $ISO_PATH"
-else
-    echo "Downloading ISO from $ISO_URL..."
-    curl -L -o "$ISO_PATH" "$ISO_URL"
-fi
+for image in "${!IMAGES[@]}"; do
+  IFS="|" read -r url <<<"${IMAGES[$image]}"
+  clean_url="${url%%\?*}"
+  filename="${clean_url##*/}"
+  target_file="${DOWNLOAD_DIR}/${filename}"
+  image_file="${CACHE_DIR}/${file}"
+ 
+  if [ -f "$image_file" ]; then
+    echo "ISO already exists at $image_file"
+  else
+    echo "Downloading ISO from $url..."
+    curl -s -o "$target_file" "$url"
+  fi
 
-# if is_compressed "$ISO_PATH"; then
-#   case "$ISO_PATH" in
-#     *.bz2)
-#       bunzip2 -c "$ISO_PATH" >"$ISO_PATH"
-#       ;;
-#     *)
-#       echo "ERROR: Unsupported compression type"
-#       exit 1
-#       ;;
-#   esac
-# fi
+  if is_compressed "$target_file"; then
+    case "$target_file" in
+      *.bz2)
+        bunzip2 -c "$target_file" >"$image_file"
+        ;;
+      *.xz)
+        # TODO: decompress xz
+        ;;
+      *)
+        echo "ERROR: Unsupported compression type"
+        exit 1
+        ;;
+    esac
+  fi
 
-echo "Verifying checksum..."
-ACTUAL_CHECKSUM=$(sha256sum "$ISO_PATH" | awk '{print $1}')
-#ACTUAL_CHECKSUM=$(get_checksum "$ISO_PATH")
+  # echo "Verifying checksum..."
+  # ACTUAL_CHECKSUM=$(sha256sum "$ISO_PATH" | awk '{print $1}')
+  # #ACTUAL_CHECKSUM=$(get_checksum "$ISO_PATH")
+  #
+  # if [ "$ACTUAL_CHECKSUM" != "$EXPECTED_CHECKSUM" ]; then
+  #   echo "Checksum mismatch!"
+  #   echo "Expected: $EXPECTED_CHECKSUM"
+  #   echo "Actual:   $ACTUAL_CHECKSUM"
+  #   #exit 1
+  # fi
+  #
+  # echo "Checksum verified"
 
-if [ "$ACTUAL_CHECKSUM" != "$EXPECTED_CHECKSUM" ]; then
-    echo "Checksum mismatch!"
-    echo "Expected: $EXPECTED_CHECKSUM"
-    echo "Actual:   $ACTUAL_CHECKSUM"
-    #exit 1
-fi
+done
 
-echo "Checksum verified"
