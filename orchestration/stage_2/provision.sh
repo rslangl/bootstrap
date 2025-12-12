bc7d48a0a699c3977642e988f1c724e93e0a14d8
rslangl <151566232+rslangl@users.noreply.github.com>
2025-12-12 22:39:58 +0100

    refactor: outline for ansible collection and orchestrator for layered provisioning (#95)
    * refactor: outline for ansible collection and orchestrator for layered provisioning
    
    * fix: lint
    
    * refactor: rename from build to validate
    
    * fix: lint
    
    * chore: cleanup
    
    * chore: fix lint and temporarily disable build

---
+++ orchestration/stage_2/provision.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Sets up the second layer of the infrastructure after the first level of orchestration is
+# complete, primarily the virtualization layer on which the actual applications will 
+# run on
+
+set -euo pipefail
+
+# TODO: setup VMs on hypervisors
